580e017ba4e2f03b123cc43cf067a4d8b024f904
Rahul Rampal <52612621+Rahul-Rampal0804@users.noreply.github.com>
2024-04-03 22:24:17 -0400

    Update server.js

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -10,7 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // MongoDB Connection
